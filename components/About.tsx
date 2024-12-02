55532876bee8a29f53946e2174ed57537b3102bf
Arkadyuti <arkadyutidas122@gmail.com>
2024-12-02 22:57:05 +0530

    Implemented svg

---
+++ components/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Background from './Background'
+
+export default function About() {
+  return (
+    <>
+    <div className='border-2 border-slate-900 h-dvh relative z-20'>
+      <p>Some content to be here</p>
+    </div>
+      <Background/>
+    </>
+  )
+}
---
+++ components/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='border-2 border-green-900 h-dvh'>
+      <p>Home content</p>
+    </div>
+  )
+}
