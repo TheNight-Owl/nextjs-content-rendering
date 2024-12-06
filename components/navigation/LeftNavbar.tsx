1e2a016c4302838bae35519d4151015a4453af66
Cienias <cieja0@gmail.com>
2024-12-06 11:03:19 +0100

    feat: add seamless vertical background do navbar

--- components/navigation/LeftNavbar.tsx
+++ components/navigation/LeftNavbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LeftNavbar = () => {
   return (
-    <div className="bg-green-300 sticky left-0 top-0 w-fit h-dvh z-50 px-4 flex flex-col justify-evenly">
+    <div className="bg-vertical-waves bg-contain sticky left-0 top-0 w-fit h-dvh z-50 px-4 flex flex-col justify-evenly">
       LeftNavbar
       <div className="flex flex-col">
         <div>Link</div>
